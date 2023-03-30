e6315c1898ece8564417252376e8f27c12e68c0c
Théo <la217960@student.helha.be>
2023-03-30 16:25:07 +0200

    visuel main

---
+++ src/main/java/com/example/boucherie/Views/LeftViewController.java
@@ -0,0 +1,11 @@
+package com.example.boucherie.Views;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+public class LeftViewController  {
+
+    @FXML
+    private AnchorPane anchorPane;
+}
